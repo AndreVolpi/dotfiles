6723f75ef2cc11fdbef8d1fcb779326f285e2c7a
Andre Volpi <devolpi@gmail.com>
2024-06-28 16:45:26 +0100

    feat: Use onedark theme for Nvim airline

--- home/nvim/config/airline.nix
+++ home/nvim/config/airline.nix
@@ -2,10 +2,12 @@
   programs.neovim = {
     plugins = with pkgs.vimPlugins; [
       airline
+      vim-airline-themes
     ];
 
     extraLuaConfig = /* lua */ ''
       vim.g.airline_powerline_fonts = true
+      vim.g.airline_theme = 'onedark'
     '';
   };
 }
