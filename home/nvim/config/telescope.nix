494ced4d93f4ad6bb5c76bc450b5cafff362709f
André Volpi <devolpi@gmail.com>
2024-07-11 11:11:23 +0100

    feat: Show hidden files while ignoring node_modules

--- home/nvim/config/telescope.nix
+++ home/nvim/config/telescope.nix
@@ -6,8 +6,17 @@
 
     extraLuaConfig = /* lua */ ''
       require('telescope').setup({
-        defaults = { display_path = 'smart' },
+        defaults = {
+          display_path = 'smart',
+          find_command = { "rg", "--color=never", "--no-heading", "--with-filename", "--line-number", "--column", "--smart-case", "--hidden", "--glob", "!**/.git/*", "--glob", "!**/node_modules/*" },
+        },
         extensions = { fzf = { case_mode = 'smart_case', fuzzy = true } },
+        pickers = {
+          fd = {
+            -- `hidden = true` will still show the inside of `.git/` as it's not `.gitignore`d.
+            find_command = { "rg", "--files", "--hidden", "--glob", "!**/.git/*", "--glob", "!**/node_modules/*" },
+          },
+        },
       })
 
       vim.keymap.set('n', '<C-p>', '<cmd>Telescope fd<cr>')
