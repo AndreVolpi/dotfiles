6a9bc4f8edec2a21cb8deadd0b6075a5b9692878
André Volpi <devolpi@gmail.com>
2025-05-15 23:39:53 +0100

    refa: Alphavetically order imports and packages

--- home/default.nix
+++ home/default.nix
@@ -11,39 +11,38 @@
     ./tmux.nix
   ];
 
+  programs.home-manager.enable = true;
+  home.stateVersion = "24.11";
+
   home.username = "kurumas";
   home.homeDirectory = "/home/kurumas";
 
-  home.stateVersion = "24.11";
-
   news.display = "show";
-
-  nixpkgs.config.allowUnfree = true;
-
-  home.packages = with pkgs; [
-    gnumake
-    gnupg
-    binutils
-    moreutils
-    coreutils
-    less
-    ripgrep
-    fd
-    jq
-    wget
-    bat
-    htop
-    openssh
-    cascadia-code
-    gping
-    docker
-    docker-compose
-  ];
 
   home.file = { };
   home.sessionVariables = { };
 
-  programs.home-manager.enable = true;
+  nixpkgs.config.allowUnfree = true;
+
+  home.packages = with pkgs; [
+    bat
+    binutils
+    cascadia-code
+    coreutils
+    docker
+    docker-compose
+    fd
+    gnumake
+    gnupg
+    gping
+    htop
+    jq
+    less
+    moreutils
+    openssh
+    ripgrep
+    wget
+  ];
 
   programs.bash = {
     enable = true;
